7f39f87ae9e5158ba84641cba1bd58c0dcb9ad29
Arek Goleń <arek125@gmail.com>
2021-03-20 15:49:05 +0100

    Rasp 4B support

--- Adafruit_DHT/platform_detect.py
+++ Adafruit_DHT/platform_detect.py
@@ -94,6 +94,7 @@ def pi_version():
     # Match a line like 'Hardware   : BCM2709'
     match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo,
                       flags=re.MULTILINE | re.IGNORECASE)
+    print match.group(1)
     if not match:
         # Couldn't find the hardware, assume it isn't a pi.
         return None
@@ -108,6 +109,9 @@ def pi_version():
         return 3
     elif match.group(1) == 'BCM2837':
         # Pi 3b+
+        return 3
+    elif match.group(1) == 'BCM2711':
+        # Pi 4b
         return 3
     else:
         # Something else, not a pi.
